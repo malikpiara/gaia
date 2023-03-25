6f8d5031644116c09db9794661a0d6ad0e8e94b7
Malik Piara <malikpiara@gmail.com>
2023-03-25 19:03:27 +0100

    Reading and displaying the page title and url.

--- popup.js
+++ popup.js
@@ -1,1 +1,16 @@
-console.log("This is a popup.");
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    let activeTab = tabs[0];
+    let url = activeTab.url;
+    let title = activeTab.title;
+
+    // Accessing the DOM to the url and title.
+    const contentDiv = document.getElementById('content');
+    const titleElement = document.createElement('h2');
+    const urlElement = document.createElement('h2');
+
+    titleElement.textContent = title;
+    urlElement.textContent = url;
+    
+    contentDiv.appendChild(titleElement);
+    contentDiv.appendChild(urlElement);
+  });
