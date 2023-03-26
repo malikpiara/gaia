fb5a7cd25f4eb8bc8f201db49526d5c7bebc50d3
Malik Piara <malikpiara@gmail.com>
2023-03-26 01:26:25 +0100

    Enabled link removal through toggle.

--- popup.js
+++ popup.js
@@ -5,6 +5,29 @@ function appendToDOM(elementType, content) {
     contentDiv.appendChild(element);
 }
 
+// Store the URL in local storage and Retrieve the list of stored URLs
+function toggleSavedLink(url) {
+    chrome.storage.local.get(['savedLinks'], function(result) {
+        const savedLinks = new Set(result.savedLinks || []);
+        
+        if (savedLinks.has(url)) {
+            savedLinks.delete(url);
+            console.log('URL remoed of local storage.');
+        } else {
+            savedLinks.add(url);
+            console.log('URL saved to local storage.');
+        }
+
+        const updatedLinks = Array.from(savedLinks);
+
+        chrome.storage.local.set({ savedLinks: updatedLinks }, function() {
+            console.log('URL saved to local storage.');
+        });
+
+        console.log('List of stored URLS', updatedLinks);
+    });
+};
+
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     // Get URL and title of active tab
     const activeTab = tabs[0];
@@ -14,19 +37,6 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     appendToDOM('h2', title);
     appendToDOM('h3', url);
 
-    // Store the URL in local storage and Retrieve the list of stored URLs
-    chrome.storage.local.get(['savedLinks'], function(result) {
-        const savedLinks = result.savedLinks || [];
-
-        if (!savedLinks.includes(url)) {
-            savedLinks.push(url);
-        }
-
-        chrome.storage.local.set({ savedLinks }, function() {
-            console.log('URL saved to local storage.');
-        })
-
-        console.log('List of stored URLS', savedLinks);
-    })
+    toggleSavedLink(url);
     //chrome.tabs.create({ url: chrome.runtime.getURL("links.html") });
   });
